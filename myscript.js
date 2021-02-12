cd63c2f6ed36a393297f0ff6a09f3b29899b6a25
Amanikashema <61458926+Amanikashema@users.noreply.github.com>
2021-02-12 04:23:54 -0500

    Add files via upload
    responsive update

--- myscript.js
+++ myscript.js
@@ -16,7 +16,7 @@
 function myFunction() {
     var x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
-      x.className += " responsive";
+      x.className += "toggle-button";
     } else {
       x.className = "topnav";
     }
